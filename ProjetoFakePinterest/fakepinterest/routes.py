11190d8fa95f68fd8af9101cf399fb3c98d9383a
Davi <oversouls11@gmail.com>
2024-05-01 20:34:47 -0300

    Primeiro Commit

---
+++ ProjetoFakePinterest/fakepinterest/routes.py
@@ -0,0 +1,84 @@
+# criar rotas do site (os links)
+
+import os
+from flask import render_template, url_for, redirect
+from fakepinterest import app, database, bcrypt
+from flask_login import login_required, login_user, logout_user, current_user
+from fakepinterest.forms import FormLogin, FormCriarConta, FormFoto
+from fakepinterest.models import Usuario, Foto
+from werkzeug.utils import secure_filename
+
+@app.route("/", methods=["GET", "POST"])
+def homepage():
+    formlogin = FormLogin()
+    if formlogin.validate_on_submit():
+        # buscando usuário no banco de acordo com o primeiro login encontrado
+        usuario = Usuario.query.filter_by(email=formlogin.email.data).first()
+
+        # caso ache vai checar a senha em hash do usuário
+        if usuario and bcrypt.check_password_hash(usuario.senha, formlogin.senha.data):
+            login_user(usuario)
+            return redirect(url_for("perfil", id_usuario=usuario.id))
+
+    return render_template("homepage.html", form=formlogin)
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    if formcriarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(formcriarconta.senha.data)
+
+        # para descriptografar é utilizado o bcrypt.check_password_hash()
+
+        usuario = Usuario(username=formcriarconta.username.data,
+                          email=formcriarconta.email.data,
+                          senha=senha)
+
+        database.session.add(usuario)
+        database.session.commit()
+
+        # realiza o login e armazena cookies no navegador
+        login_user(usuario, remember=True)
+
+        # redirecionando usuário para tela de perfil
+        return redirect(url_for("perfil", id_usuario=usuario.id))
+
+    return render_template("criarconta.html", form=formcriarconta)
+
+@app.route("/perfil/<id_usuario>", methods=["GET", "POST"])
+@login_required
+def perfil(id_usuario):
+    if int(id_usuario) == int(current_user.id):
+        # o usuário está visualizando o próprio perfil
+        form_foto = FormFoto()
+        if form_foto.validate_on_submit():
+            arquivo = form_foto.foto.data
+            # deixando o nome seguro para ser armazenado no servidor
+            nome_seguro = secure_filename(arquivo.filename)
+            # salvar arquivo na pasta fotos_posts
+            caminho = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                              app.config["UPLOAD_FOLDER"],
+                              nome_seguro)
+            arquivo.save(caminho)
+            # registrar esse arquivo no banco de dados
+            foto = Foto(imagem=nome_seguro, id_usuario=current_user.id)
+            database.session.add(foto)
+            database.session.commit()
+        return render_template("perfil.html", usuario=current_user, form=form_foto)
+    else:
+        usuario = Usuario.query.get(int(id_usuario))
+        return render_template("perfil.html", usuario=usuario, form=None)
+
+@app.route("/logout")
+@login_required
+def logout():
+    # deslogando usuário logado no momento
+    logout_user()
+    return redirect(url_for("homepage"))
+
+@app.route("/feed")
+@login_required
+def feed():
+    fotos = Foto.query.order_by(Foto.data_criacao.desc()).all()
+    return render_template("feed.html", fotos=fotos)
+
---
+++ ProjetoFakePinterest/main.py
@@ -0,0 +1,8 @@
+
+from fakepinterest import app
+
+# lembrete criar ambiente virtual sempre pelo vscode, depois selecionar interpretador por aqui
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
