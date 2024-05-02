fab06d8d2a377ae9be019026f109898cde519abd
Davi <oversouls11@gmail.com>
2024-05-01 21:24:10 -0300

    Colocando Banco em Nuvem

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "182ff69e8b2724961bc5a2067fe02c2b"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
